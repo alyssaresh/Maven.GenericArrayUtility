2e6827fc6d5f723ea9796f6992dc2f072cdc9398
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-11-30 17:37:18 -0500

    get most common working

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,8 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -10,7 +10,6 @@ import java.util.List;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
-
     private T[] inputArray;
 
     public ArrayUtility(T[] input) {
@@ -18,36 +17,49 @@ public class ArrayUtility<T> {
     }
 
 
+//    Integer expected = 7;
+//    Integer[] inputArray = {1, 2, expected, 8, 4, 5, expected, 0, 9, 8, expected};
+//    Integer[] arrayToMerge = {1, 2, expected, 8, 4, 5, expected, 0, 9, 8};
+//    ArrayUtility<Integer> arrayUtility = new ArrayUtility<Integer>(inputArray);
+//
+//    // When
+//    Integer actual = arrayUtility.getMostCommonFromMerge(arrayToMerge);
+
     public int countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        T value = valueToEvaluate;
-        ArrayList<T> dupes = new ArrayList<>(Arrays.asList(inputArray));
-        dupes.addAll(Arrays.asList(arrayToMerge));
-        int duplicates = 0;
-        for (T t: dupes){
-            if (t == value) duplicates++;
-        } return duplicates;
+        T[] mergedArray = (T[]) Array.newInstance(inputArray.getClass().getComponentType(), inputArray.length + arrayToMerge.length);
+        System.arraycopy(inputArray, 0, mergedArray, 0, inputArray.length);
+        System.arraycopy(arrayToMerge, 0, mergedArray, inputArray.length, arrayToMerge.length);
+        int totalCount = 0;
+        for (T t : mergedArray) {
+            if (t == valueToEvaluate) totalCount++;
+        }
+        return totalCount;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        ArrayList<T> merge = new ArrayList<>(Arrays.asList(inputArray));
-        merge.addAll(Arrays.asList(arrayToMerge));
-        System.out.print(merge);
-        int max = 0;
-        T maxObj = null;
-        int currentNumTotal = 0;
-        for (int i = 0; i < merge.size(); i++){
-            T currentNum = merge.get(i);
-            for (T t : merge){
-                if (currentNum == t) {
-                    currentNumTotal++;
+        T[] mergedArray = (T[]) Array.newInstance(inputArray.getClass().getComponentType(), inputArray.length + arrayToMerge.length);
+        System.arraycopy(inputArray, 0, mergedArray, 0, inputArray.length);
+        System.arraycopy(arrayToMerge, 0, mergedArray, inputArray.length, arrayToMerge.length);
+        int maxCount = 0;
+        T mostFrequentElement = null;
+        for (int i = 0; i < mergedArray.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mergedArray.length; j++) {
+                if (mergedArray[j] == mergedArray[i]) {
+                    count++;
                 }
-            } if (currentNumTotal>max){
-                maxObj = currentNum;
-                max = currentNumTotal;
             }
-        } return maxObj;
+            if (count > maxCount) {
+                maxCount = count;
+                mostFrequentElement = mergedArray[i];
+            }
+        }
+        return mostFrequentElement;
     }
 }
+
+
+
 
 
 //can't figure out why inputArray keeps returning as null, can't get getmostcommon to work
